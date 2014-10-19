6ac0f6b1943836093a75f8858def596c31443294
Marcus Schorow <mschorow@college.harvard.edu>
2014-10-18 20:56:28 -0400

    error handling

--- bot.py
+++ bot.py
@@ -58,6 +58,10 @@ if __name__ == "__main__":
         responses = responses['data']
         responses = filter(lambda x:x['polarity']==0, responses)
         for response in responses:
-            twitter.tweet(response['user']+": "+response['text'])
-            twitter.tweet(response['user']+" I'm sorry you didn't enjoy "+competitor+". Have you considered switching to "+company+"?")
+            try:
+                twitter.tweet(response['user']+": "+response['text'])
+                twitter.tweet(response['user']+" I'm sorry you didn't enjoy "+competitor+". Have you considered switching to "+company+"?")
+                break
+            except tweepy.error.TweepError:
+                print "whoops"
         time.sleep(60) #1 minute
