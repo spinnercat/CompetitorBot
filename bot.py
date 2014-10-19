99eaa649c8fbce80737f76139548b09c7367b011
Marcus Schorow <mschorow@college.harvard.edu>
2014-10-18 20:18:57 -0400

    Working with comcast/directv

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import time
 
 import tweepy
+import json
+import requests
 
 class TwitterAPI(object):
     """
@@ -26,7 +28,32 @@ class TwitterAPI(object):
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
-    twitter.tweet("This is a test tweet!") #You probably want to remove this line
+    api = twitter.api
+    competitor = "Comcast"
+    company = "DirecTV"
+    queryString = competitor + " -" + company
+    latest_id = 0
     while True:
         #Send a tweet here!
-        time.sleep(60)
+        tweets = api.search(q=queryString, rpp=50, show_user=True, lang="en", since_id = latest_id)
+        sentimentQueries = []
+        for tweet in tweets:
+            latest_id = max(latest_id, tweet.id)
+            text = tweet.text
+            sentimentQuery = {}
+            sentimentQuery['text'] = text
+            sentimentQuery['query'] = competitor
+            sentimentQuery['user'] = tweet.user.name
+            if not tweet.user.name.lower() == competitor.lower():
+                sentimentQueries.append(sentimentQuery)
+        sentimentQueries = {'data':sentimentQueries}
+        sentimentQueries = json.dumps(sentimentQueries)
+        r = requests.post(url="http://www.sentiment140.com/api/bulkClassifyJson?mschorow@college.harvard.edu",
+                      data=sentimentQueries)
+        responses = json.loads(r.text)
+        responses = responses['data']
+        responses = filter(lambda x:x['polarity']==0, responses)
+        for response in responses:
+            twitter.tweet(response['user']+": "+response['text'])
+            twitter.tweet(response['user']+" I'm sorry you didn't enjoy "+competitor+". Have you considered switching to "+company+"?")
+        time.sleep(60) #1 minute
